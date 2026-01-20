b2987618cab987d6db2276481b4ef140cadcd45c
sigumaocsjoho <242105@ocsjoho.onmicrosoft.com>
2026-01-20 11:19:25 +0900

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,12 +1,19 @@
 <?php
-$dsn = 'mysql:host=maglev.proxy.rlwy.net;dbname=railway;port=18346;charset=utf8mb4';
-$user = 'root';
-$pass = 'TCYxGMGvhaPftphOaiJOAtMtbSYHePCa';
-
 try {
-    $pdo = new PDO($dsn, $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "mysql:host=" . getenv('DB_HOST')
+         . ";port=" . getenv('DB_PORT')
+         . ";dbname=" . getenv('DB_NAME')
+         . ";charset=utf8mb4";
+
+    $pdo = new PDO(
+        $dsn,
+        getenv('DB_USER'),
+        getenv('DB_PASS'),
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+
     echo "DB接続成功";
+
 } catch (PDOException $e) {
     exit('DB接続エラー: ' . $e->getMessage());
 }
